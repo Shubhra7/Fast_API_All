3849a6c0f68d939c39b23b20b1f553aee962964b
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-08-04 11:55:42 +0530

    CRUD fucntion using Fast api done

--- main.py
+++ main.py
@@ -8,3 +8,37 @@ class Tea(BaseModel):
     id: int
     name:str
     origin: str
+
+teas: List[Tea] = []
+
+# decorator give extra power to your class/fucntion/service
+# app decorator to handle api
+
+@app.get("/")
+def read_root():
+    return {"message" : "Welcome to Shubhra Fast API..!"}
+
+@app.get("/teas")
+def get_teas():
+    return teas
+
+@app.post("/teas")
+def add_tea(tea: Tea):
+    teas.append(tea)
+    return tea
+
+@app.put("/teas/{tea_id}")
+def update_tea(tea_id:int, updated_tea: Tea):
+    for index,tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index]=updated_tea
+            return updated_tea
+    return {"error": "Tea not found!!"}
+
+@app.delete("/teas/{tea_id}")
+def delete_tea(tea_id:int):
+    for index,tea in enumerate(teas):
+        if tea.id == tea_id:
+            deleted = teas.pop(index)
+            return deleted
+    return {"error": "Tea not found!!"}
